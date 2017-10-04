ff86c88cb9361629a53ff876cba7004b18c4b712
Kazu <kazu-@live.fr>
2017-10-04 20:27:41 +0200

    c

--- bot.js
+++ bot.js
@@ -12,11 +12,6 @@ var bot = new Discord.Client({
    token: auth.token,
    autorun: true
 });
-function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min +1)) + min;
-}
 
 
 bot.on('ready', function (evt) {
@@ -26,70 +21,73 @@ bot.on('ready', function (evt) {
 });
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Prevent bot to answer itself
-    if (message.auth.bot) return;
-    // It will listen for messages that will start with `§`
-    if (message.substring(0, 1) == '§') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
-        args = args.splice(1);
-        switch(cmd) {
-            // §tableflip
-            case 'tableflip':
-				bot.sendMessage({
-					to: channelID,
-					message: '(ノಠ益ಠ)ノ彡┻━┻'
-				});
-            break;
-            // §unflip
-			case 'unflip':
-				bot.sendMessage({
-					to: channelID,
-					message: '┬──┬ ノ( ゜-゜ノ)'
-				});
-            break;
-            // §stupid
-			case 'stupid':
-				bot.sendMessage({
-					to: channelID,
-					message: 'I am the almighty pangolin-bot. You are all stupid.'
-				});
-            break;
-            // §omae
-			case 'omae':
-				bot.sendMessage({
-					to: channelID,
-					message: 'NANI ?!?'
-				});
-			break;
-         }
-     }
+    if (message.author.bot) {
+        //nothing !
+    } else {
+        // It will listen for messages that will start with `§`
+        if (message.substring(0, 1) == '§') {
+            var args = message.substring(1).split(' ');
+            var cmd = args[0];
+        
+            args = args.splice(1);
+            switch(cmd) {
+                // §tableflip
+                case 'tableflip':
+                    bot.sendMessage({
+                        to: channelID,
+                        message: '(ノಠ益ಠ)ノ彡┻━┻'
+                    });
+                break;
+                // §unflip
+                case 'unflip':
+                    bot.sendMessage({
+                        to: channelID,
+                        message: '┬──┬ ノ( ゜-゜ノ)'
+                    });
+                break;
+                // §stupid
+                case 'stupid':
+                    bot.sendMessage({
+                        to: channelID,
+                        message: 'I am the almighty pangolin-bot. You are all stupid.'
+                    });
+                break;
+                // §omae
+                case 'omae':
+                    bot.sendMessage({
+                        to: channelID,
+                        message: 'NANI ?!?'
+                    });
+                break;
+            }
+        }
 
-     if (message.toLowerCase().includes('omae wa mo shindeiru')) {
-         bot.sendMessage({
-             to: channelID,
-             message: 'NANI ?!'
-         });
-     }
-
-     if ((message.toLowerCase().includes('chine'))
-        || message.toLowerCase().includes('tine') 
-        || message.toLowerCase().includes('chinois') 
-        || message.toLowerCase().includes('chinoise') 
-        || message.toLowerCase().includes('tinois')
-        || message.toLowerCase().includes('tinoise')) {
-         bot.sendMessage({
-             to: channelID,
-             message: 'Tine ? Tinois ? Ping pong mahjong dugong !'
-         });
-     }
-
-     if (message.toLowerCase().includes('gnap')) {
-         if ( Math.floor(Math.random() * (1000 + 1)) > 950) {
+        if (message.toLowerCase().includes('omae wa mo shindeiru')) {
             bot.sendMessage({
                 to: channelID,
-                message: 'Attention, cas de rage potentiel, prenez garde aux gnappeurs !'
+                message: 'NANI ?!'
             });
-         }
-     }
+        }
+
+        if ((message.toLowerCase().includes('chine'))
+            || message.toLowerCase().includes('tine') 
+            || message.toLowerCase().includes('chinois') 
+            || message.toLowerCase().includes('chinoise') 
+            || message.toLowerCase().includes('tinois')
+            || message.toLowerCase().includes('tinoise')) {
+            bot.sendMessage({
+                to: channelID,
+                message: 'Tine ? Tinois ? Ping pong mahjong dugong !'
+            });
+        }
+
+        if (message.toLowerCase().includes('gnap')) {
+            if ( Math.floor(Math.random() * (1000 + 1)) > 950) {
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Attention, cas de rage potentiel, prenez garde aux gnappeurs !'
+                });
+            }
+        }
+    }
 });
