7ba6688710f3cc58315d10615764c4f8f5a9de5b
Kazu <kazu-@live.fr>
2017-10-04 19:31:23 +0200

    doc

--- bot.js
+++ bot.js
@@ -18,39 +18,42 @@ bot.on('ready', function (evt) {
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
 bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
+    // It will listen for messages that will start with `§`
     if (message.substring(0, 1) == '§') {
         var args = message.substring(1).split(' ');
         var cmd = args[0];
        
         args = args.splice(1);
         switch(cmd) {
-            // !ping
+            // §ping
             case 'ping':
                 bot.sendMessage({
                     to: channelID,
                     message: 'Pong!'
                 });
             break;
+            // §tableflip
             case 'tableflip':
 				bot.sendMessage({
 					to: channelID,
 					message: '(ノಠ益ಠ)ノ彡┻━┻'
 				});
-			break;
+            break;
+            // §unflip
 			case 'unflip':
 				bot.sendMessage({
 					to: channelID,
 					message: '┬──┬ ノ( ゜-゜ノ)'
 				});
-			break;
+            break;
+            // §stupid
 			case 'stupid':
 				bot.sendMessage({
 					to: channelID,
 					message: 'I am the almighty pangolin-bot. You are all stupid.'
 				});
-			break;
+            break;
+            // §omae
 			case 'omae':
 				bot.sendMessage({
 					to: channelID,
